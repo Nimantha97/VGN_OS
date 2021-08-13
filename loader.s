897265ebf44733f42a612172dc8fd65a2e480683
Nimantha Gayan <73300218+Nimantha97@users.noreply.github.com>
2021-08-13 23:39:58 +0530

    Integrate segmentation (#4)
    * add  Makefile
    
    * bochslog.txt
    
    * frame_buffer.h
    
    * frame_buffer.o
    
    * iso/boot/kernel.elf
    
    * kernel.elf
    
    * kmain.c
    
    * kmain.o
    
    * add loader.o
    
    * add loader.s
    
    * add os.iso
    
    * add gdt.c
    
    * add gdt.h
    
    * add gdt.o
    
    * add gdt_asm.o
    
    * add gdt_asm.s
    
    * add types.h

--- loader.s
+++ loader.s
@@ -1,43 +1,33 @@
-    global loader                   ; the entry symbol for ELF
-    extern sum_of_three	            ; kmain file is extrenal
+    global loader                   
 
-    
-    MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-    FLAGS        equ 0x0            ; multiboot flags
-    CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
-                                    ; (magic number + checksum + flags should equal 0) 
-    
-    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
-     
+    MAGIC_NUMBER equ 0x1BADB002     
+    FLAGS        equ 0x0            
+    CHECKSUM     equ -MAGIC_NUMBER  
+                                    
+    KERNEL_STACK_SIZE equ 4096      
+
     section .bss
-    align 4                                     ; align at 4 bytes
-    kernel_stack:                               ; label points to beginning of memory
-        resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel                              
+    align 4                         
+    kernel_stack:                   
+        resb KERNEL_STACK_SIZE      
 
-    section .text:                  ; start of the text (code) section
-    align 4                         ; the code must be 4 byte aligned
-        dd MAGIC_NUMBER             ; write the magic number to the machine code,
-        dd FLAGS                    ; the flags,
-        dd CHECKSUM                 ; and the checksum
+    section .text:                  
+    align 4                         
+        dd MAGIC_NUMBER             
+        dd FLAGS                    
+        dd CHECKSUM                 
 
-   
-   
-    loader:                         ; the loader label (defined as entry point in linker script)
-        
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
-        mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
-                                                ; stack (end of memory area)
-	 
-	
-	push dword 3
-    	push dword 2
-    	push dword 1
-    	call sum_of_three
+    loader:                         
+        mov esp, kernel_stack + KERNEL_STACK_SIZE       
+                                                                                        
+       
+        extern kmain 
+        mov eax, 0xAB
+        mov eax, 0xCD
+        call kmain    
 
     .loop:
-        jmp .loop                   ; loop forever
-        
-   
+        jmp .loop             
 
     
         

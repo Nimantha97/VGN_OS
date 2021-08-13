effbc0827cbad5d3cd46cd07c819f2b8f95f3050
Nimantha97 <nimanthagayan0309@gmail.com>
2021-08-13 23:20:45 +0530

    kmain.c

--- kmain.c
+++ kmain.c
@@ -1,14 +1,21 @@
  #include "frame_buffer.h"
 #include "serial_port.h"
-
-/* The C function */
-int sum_of_three(int a, int b, int c) {
-  char buffer[25] = "HELLO OS";
-  fb_write(buffer, 9);
-  serial_configure(SERIAL_COM1_BASE, Baud_115200);
-  serial_write(SERIAL_COM1_BASE, buffer, 26);
-  return a + b + c;
-}
+#include "gdt.h"
+void init() {
  
+  init_gdt();
+
+  
+  serial_configure(SERIAL_COM1_BASE, Baud_115200);
+}
+
+
+int kmain() {
+  init();
+  char buffer[20] = "Welcome to kmOS\n";
+  fb_write(buffer, 20);
+  serial_write(SERIAL_COM1_BASE, buffer, 20);
+  return 0;
+}
  
  
